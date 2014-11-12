f49923997bc6547306c0e98bf5aa3536e8ddda7a
Vladimir Yashunsky <yvd@lscom.ru>
2014-11-12 16:46:25 +0300

    auth implemented

---
+++ db_funcs.py
@@ -0,0 +1,213 @@
+from hashlib import sha512
+
+SALT = 'GameNetSalt'
+
+def salt_password(password):
+    salted = (SALT + password).encode('utf-8')
+    return sha512(salted).hexdigest()
+
+def create_tables(connection):
+    cursor = connection.cursor()
+    query = '''
+        CREATE TABLE `users` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `name` TEXT NOT NULL,
+            `password` TEXT NOT NULL
+            );
+        '''
+    cursor.execute(query)
+
+    query = '''
+        CREATE TABLE `groups` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `name` TEXT NOT NULL
+            );
+        '''
+    cursor.execute(query)
+
+    query = '''
+        CREATE TABLE `membership` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `user_id` INTEGER NOT NULL,
+            `group_id` INTEGER NOT NULL
+            );
+        '''
+    cursor.execute(query)
+
+    query = '''
+        CREATE TABLE `tags` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `name` TEXT,
+            `parent_id`
+            );
+        '''
+    cursor.execute(query)
+
+    query = '''
+        CREATE TABLE `documents` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `sender_id` INTEGER,
+            `user_recipient_id` INTEGER,
+            `group_recipient_id` INTEGER,
+            `access_password` TEXT,
+            `header` TEXT,
+            `data` TEXT
+            );
+        '''
+    cursor.execute(query)
+
+    query = '''
+        CREATE TABLE `doc_tags` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `document_id` INTEGER NOT NULL,
+            `tag_id` INTEGER NOT NULL
+            );
+        '''
+    cursor.execute(query)
+
+    query = '''
+        CREATE TABLE `access_log` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `document_id` INTEGER NOT NULL,
+            `user_id` INTEGER NOT NULL,
+            `action` TEXT
+            );
+        '''
+    cursor.execute(query)
+
+    query = '''
+        CREATE TABLE `user_access` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `user_id` INTEGER NOT NULL,
+            `tag_id` INTEGER NOT NULL,
+            `read` TEXT,
+            `write` TEXT,
+            `view_log` TEXT,
+            `delete_log` TEXT,
+            `modify_log` TEXT,
+            `view_header` TEXT
+            );
+        '''
+    cursor.execute(query)
+
+    query = '''
+        CREATE TABLE `group_access` (
+            `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            `group_id` INTEGER NOT NULL,
+            `tag_id` INTEGER NOT NULL,
+            `read` TEXT,
+            `write` TEXT,
+            `view_log` TEXT,
+            `delete_log` TEXT,
+            `modify_log` TEXT,
+            `view_header` TEXT
+            );
+        '''
+    cursor.execute(query)
+
+    connection.commit()
+
+def add_user(connection, username, password):
+    password = salt_password(password)
+    cursor = connection.cursor()
+
+    query = '''
+        INSERT INTO `users` (`name`, `password`)
+        VALUES (?, ?);
+        '''
+    cursor.execute(query, (username, password))
+
+    connection.commit()
+
+    return cursor.lastrowid
+
+def add_group(connection, name):
+    cursor = connection.cursor()
+
+    query = '''
+        INSERT INTO `groups` (`name`)
+        VALUES (?);
+        '''
+    cursor.execute(query, name)
+
+    connection.commit()
+
+    return cursor.lastrowid
+
+def add_membership(connection, user_id, group_id):
+    cursor = connection.cursor()
+
+    query = '''
+        INSERT INTO `membership` (`user_id`, `group_id`)
+        VALUES (?, ?);
+        '''
+    cursor.execute(query, (user_id, group_id))
+
+    connection.commit()
+
+    return cursor.lastrowid
+
+def add_tag(connection, name, parent_id):
+    cursor = connection.cursor()
+
+    query = '''
+        INSERT INTO `tags` (`name`, `parent_id`)
+        VALUES (?, ?);
+        '''
+    cursor.execute(query, (name, parent_id))
+
+    connection.commit()
+
+    return cursor.lastrowid
+
+def add_document(connection, sender_id, user_recipient_id,
+                 group_recipient_id, access_password,
+                 header, data):
+    cursor = connection.cursor()
+
+    password = salt_password(access_password)
+
+    query = '''
+        INSERT INTO `documents` (`sender_id`, `user_recipient_id`,
+                                 `group_recipient_id`,
+                                 `access_password`, `header`, `data`)
+        VALUES (?, ?, ?, ?, ?, ?);
+        '''
+    cursor.execute(query, (sender_id, user_recipient_id,
+                           group_recipient_id, password,
+                           header, data))
+
+    connection.commit()
+
+    return cursor.lastrowid
+
+def add_doc_tag(connection, document_id, tag_id):
+    cursor = connection.cursor()
+
+    query = '''
+        INSERT INTO `doc_tags` (`document_id`, `tag_id`)
+        VALUES (?, ?);
+        '''
+    cursor.execute(query, (document_id, tag_id))
+
+    connection.commit()
+
+    return cursor.lastrowid
+
+def auth(connection, username, password):
+    cursor = connection.cursor()
+    password = salt_password(password)
+
+    query = '''
+        SELECT id FROM `users`
+        WHERE `name` = ?
+        AND `password` = ?;
+    '''
+    cursor.execute(query, (username, password))
+
+    answer = cursor.fetchone()
+    
+    return str(answer[0]) if answer is not None else ''
+
+
+
---
+++ server.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+#
+# Copyright 2009 Facebook
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import tornado.auth
+import tornado.escape
+import tornado.httpserver
+import tornado.ioloop
+import tornado.web
+
+from tornado import gen
+from tornado.options import define, options, parse_command_line
+
+from sqlite3 import connect
+
+from database_setup import DB_NAME
+
+from db_funcs import auth
+
+define("port", default=8888, help="run on the given port", type=int)
+
+
+class Application(tornado.web.Application):
+    def __init__(self):
+        handlers = [
+            (r"/", MainHandler),
+            (r"/login", LoginHandler),
+            (r"/logout", LogoutHandler),
+        ]
+        settings = dict(
+            cookie_secret="__TODO:_GENERATE_YOUR_OWN_RANDOM_VALUE_HERE__",
+            login_url="/login",
+        )
+        tornado.web.Application.__init__(self, handlers, **settings)
+
+        self.db = connect(DB_NAME)
+
+
+class BaseHandler(tornado.web.RequestHandler):
+    @property
+    def db(self):
+        return self.application.db
+
+    def get_current_user(self):
+        return self.get_secure_cookie("user_id")
+
+
+class MainHandler(BaseHandler):
+    def get(self):
+        if not self.current_user:
+            self.redirect("/login")
+            return
+        user_id = tornado.escape.xhtml_escape(self.current_user)
+        items = [user_id, str(self.db), "Item 3"]
+        self.render("templates/index.html", title="My title", items=items)
+
+class LoginHandler(BaseHandler):
+    def get(self):
+        self.render("templates/login.html")
+
+    def post(self):
+        username = self.get_argument("username")
+        password = self.get_argument("password")
+
+        user_id = auth(self.db, username, password)
+
+        self.set_secure_cookie("user_id", user_id)
+        self.redirect("/")
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.set_secure_cookie("user_id", '')
+        self.redirect("/")
+
+
+def main():
+    parse_command_line()
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
